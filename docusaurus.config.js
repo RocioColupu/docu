e15a3513935a4634580828bbef074eb9d1bb184f
Roci <pprado929@gmail.com>
2024-01-31 20:55:16 -0500

    Caso 3

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://RocioColupu.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docu/',
+  baseUrl: '/caso3/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
